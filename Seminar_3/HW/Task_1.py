b096b860ab80259e2dad0bcd160c29faf5200450
Арсений <ars071026@example.com>
2023-05-26 23:15:03 +0500

    Task_1 seminar 3 HW

---
+++ Seminar_3/HW/Task_1.py
@@ -0,0 +1,15 @@
+# Требуется вычислить, сколько раз встречается некоторое число X в массиве A[1..N].
+# Пользователь в первой строке вводит натуральное число N – количество элементов в cписке .
+# В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X"
+from random import randint
+my_list = list()
+lens = int(input("Ввидите длинну списка:"))
+num = int(input("Ввидите искомое число "))
+count = 0
+for i in range(lens):
+    my_list.append(randint(0, 10))
+print(my_list)
+for i in range(len(my_list)-1):
+    if my_list[i] == num:
+        count += 1
+print(f"Число {num} встречается {count} раз")

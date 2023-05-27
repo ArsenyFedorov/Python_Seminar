eac82c421311150bd97ddcee5afb025c1b96ef02
Арсений <ars071026@example.com>
2023-05-27 12:50:46 +0500

    Task_2 seminar 3 HW

---
+++ Seminar_3/HW/Task_2.py
@@ -0,0 +1,40 @@
+# Требуется найти в массиве A[1. N] самый близкий по величине элемент к заданному числу X.
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве.
+# В последующих строках записаны N целых чисел Ai. Последняя строка содержит число X
+from random import randint
+my_list = list()
+lens = int(input("Ввидите длинну списка:"))
+num = int(input("Ввидите искомое число: "))
+for i in range(lens):
+    my_list.append(randint(0, 100))
+print(my_list)
+
+my_list = list(set(my_list))
+my_list.sort()
+print(my_list)
+if num <= my_list[0]:
+    print(f"Число {num} ближе всего к {my_list[0]}")
+elif num >= my_list[lens - 1]:
+    print(f"Число {num} ближе всего к {my_list[lens - 1]}")
+else:
+    for i in range(len(my_list)):
+        if my_list[i] > num:
+            print(f"Число {num} ближе всего к {my_list[i]}")
+            break
+        elif num < my_list[i + 1 ]:
+            if num - my_list[i] < my_list[i + 1] - num:
+                print(f"Число {num} ближе всего к {my_list[i]}")
+                break
+            else:
+                print(f"Число {num} ближе всего к {my_list[i + 1]}")
+                break
+
+
+
+
+
+
+
+
+
+
